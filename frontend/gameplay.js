9cfdf65585fad549799f534e35e3fbc88c29e9ef
unknown <maurice13stu@gmail.com>
2025-10-21 16:13:05 -0700

    .

--- frontend/gameplay.js
+++ frontend/gameplay.js
@@ -1102,9 +1102,6 @@ function setupMultiplayerReadyUI() {
 
 const DEBUG = false;
 import * as THREE from './libs/three.module.js';
-import {
-    GLTFLoader
-} from './libs/GLTFLoader.js';
 import { SkeletonUtils } from './libs/SkeletonUtils.js';
 import {
     OrbitControls
